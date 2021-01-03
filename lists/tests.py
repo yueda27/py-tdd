9cea7ab4190b164e16f1baf2e8c27e97e5a7d1a1
yueda <chua_yueda@hotmail.com>
2021-01-03 13:23:17 +0800

    updated test to use client for httprequest

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -11,10 +12,7 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
     
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-        
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
