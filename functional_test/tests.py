de9cdef7994fd88af710c78e08d62a2948a1e921
yueda <chua_yueda@hotmail.com>
2021-01-05 12:26:01 +0800

    use django test to run functional

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         options = webdriver.FirefoxOptions()
         options.headless = True
@@ -14,7 +15,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
     
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8080")
+        self.browser.get(self.live_server_url)
 
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -36,7 +37,3 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
