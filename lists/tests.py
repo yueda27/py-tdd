3626a03ab55a22a549e869751379f9a8c02553fc
yueda <chua_yueda@hotmail.com>
2021-01-07 17:31:34 +0800

    created new list obj

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -16,12 +16,15 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
     def test_saving_and_retrieving_items(self):
+        list_ = List.objects.create()
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
@@ -42,7 +45,7 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
     
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data = {'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
 
@@ -52,3 +55,9 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+class ListObjectTest(TestCase):
+    def test_can_save_list(self):
+        my_list = List.objects.create()
+        Item.objects.create(text='itemey1', list = my_list)
+        Item.objects.create(text='itemey2', list = my_list)
