7c92ad849cbcc2d71fab6368f03bc1ed3121fa51
yueda <chua_yueda@hotmail.com>
2021-01-07 18:43:38 +0800

    use list id as uri GET endpoint

--- lists/tests.py
+++ lists/tests.py
@@ -39,8 +39,24 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_use_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
+    
+    def test_displays_only_item_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey1', list = correct_list)
+        Item.objects.create(text='itemey2', list = correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text = 'other list item 1', list = other_list)
+        Item.objects.create(text = 'other list item 2', list = other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+
+        self.assertContains(response, 'itemey1')
+        self.assertContains(response, 'itemey2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list')
 
 class NewListTest(TestCase):
     
@@ -53,8 +69,8 @@ class NewListTest(TestCase):
 
     def test_redirect_after_POST(self):
         response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 class ListObjectTest(TestCase):
     def test_can_save_list(self):
--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,6 @@ from lists import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
-    url('lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
-    url('lists/new$', views.new_list, name='new_list')
+    url('lists/new$', views.new_list, name='new_list'),
+    url('lists/(.+)/$', views.view_list, name='view_list'),
 ]
