cba3b3e6fb06283c8610fc1d3eb7726df95d2a29
yueda <chua_yueda@hotmail.com>
2021-01-07 19:33:45 +0800

    added indiv url for each list

--- lists/tests.py
+++ lists/tests.py
@@ -77,3 +77,25 @@ class ListObjectTest(TestCase):
         my_list = List.objects.create()
         Item.objects.create(text='itemey1', list = my_list)
         Item.objects.create(text='itemey2', list = my_list)
+
+class NewItemTest(TestCase):
+    # def test_can_save_a_POST_request_to_existing_list(self):
+    #     other_list = List.objects.create()
+    #     correct_list = List.objects.create()
+
+    #     self.client.post(f'/list/{correct_list.id}/add_item',
+    #         data = {'item_text': 'A new item for an existing list'})
+        
+    #     #self.assertEqual(Item.objects.count(), 1)
+    #     new_item = Item.objects.first()
+    #     self.assertEqual(new_item.text, 'A new item for an existing list')
+    #     self.assertEqual(new_item.list, correct_list)
+    
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(f'/lists/{correct_list.id}/add_item', 
+            data = {'item_text': 'A new item'})
+        print(response)
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,7 @@ from lists import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
-    url('lists/new$', views.new_list, name='new_list'),
-    url('lists/(.+)/$', views.view_list, name='view_list'),
+    url(r'lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
