0f15ee3119553311a66e01e456928bc95fcd341e
yueda <chua_yueda@hotmail.com>
2021-01-14 21:18:48 +0800

    added staticroot

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -40,7 +40,7 @@ def _create_or_update_venv():
     append(".env", f'DJANGO_SECRET_KEY={new_secret}')
 
 def _update_static_files():
-    run('pipenv run python manage.py collecstatic --noinput')
+    run('pipenv run python manage.py collectstatic --noinput')
 
 def _update_database():
     run('pipenv run python manage.py migrate --noinput')
