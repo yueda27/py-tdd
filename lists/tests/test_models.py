262f05de4241da0ace06ffea7a9880180311fa56
yueda <chua_yueda@hotmail.com>
2021-01-16 10:57:26 +0800

    added error warning

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.template.loader import render_to_string
+from django.core.exceptions import ValidationError
 
 from lists.views import home_page
 from lists.models import Item, List
@@ -35,3 +36,10 @@ class HomePageTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_save_item.text, second_save_item.text)
     
+class ListAndItemTest(TestCase):
+    def test_cannot_save_empty_list(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text = '')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
 
@@ -14,7 +15,13 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST.get('item_text', ''), list = list_)
+    item = Item.objects.create(text=request.POST.get('item_text', ''), list = list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        list_.delete()
+        return render(request,"home.html", {'error': "You can't have an empty list"})
+
     return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
