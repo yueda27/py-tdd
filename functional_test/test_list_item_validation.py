e98195fea74af1deb9534226a237dc52f9e687b7
yueda <chua_yueda@hotmail.com>
2021-01-16 10:22:01 +0800

    shifted unittest to a folder list/tests

--- functional_test/test_list_item_validation.py
+++ functional_test/test_list_item_validation.py
@@ -8,6 +8,21 @@ import unittest
 import os
 
 class ItemValidationTest(FunctionalTest):
-    @unittest.skip
     def test_cannot_add_empty_list_items(self):
-        pass
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text, 
+        "You can't have an empty list item"))
+
+        self.browser.find_element_by_id('id_new_item').send_keys("Buy milk")
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text, 
+        "You can't have an empty list item"))
+
+        self.browser.find_element_by_id('id_new_item').send_keys("Make tea")
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+        self.fail("finish this test")
